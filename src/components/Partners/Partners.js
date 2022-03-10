860adfd95f9d7bda719afc1694f74a57c8d35f1f
Mariano <mariano.chomicki@inboundcycle.com>
2022-03-11 00:08:26 +0100

    partners module

---
+++ src/components/Partners/Partners.js
@@ -0,0 +1,45 @@
+import vecino from "../../assets/img/partners/vecino.png";
+import automotive from "../../assets/img/partners/automotive.png";
+import cuatrimoteando from "../../assets/img/partners/cuatrimoteando.png";
+import immorentier from "../../assets/img/partners/immorentier.png";
+import raduga from "../../assets/img/partners/raduga.png";
+import sayada from "../../assets/img/partners/sayada.png";
+import serieux from "../../assets/img/partners/serieux.png";
+import specto from "../../assets/img/partners/specto.png";
+
+const Partners = {
+  0: {
+    name: "Vecino",
+    img: vecino,
+  },
+  1: {
+    name: "Advanced Automotive",
+    img: automotive,
+  },
+  2: {
+    name: "Cuatrimoteando",
+    img: cuatrimoteando,
+  },
+  3: {
+    name: "Immorentier",
+    img: immorentier,
+  },
+  4: {
+    name: "Raduga Film LLC",
+    img: raduga,
+  },
+  5: {
+    name: "Sayada",
+    img: sayada,
+  },
+  6: {
+    name: "Sérieux",
+    img: serieux,
+  },
+  7: {
+    name: "Specto Media",
+    img: specto,
+  },
+};
+
+export { Partners };
